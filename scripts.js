91ea08b96a58774c8f7d10da506827050df3fa47
Pai thet <leontinus866@gmail.com>
2023-12-14 21:05:46 +0630

    comment js

--- scripts.js
+++ scripts.js
@@ -3,72 +3,68 @@ const form = document.querySelector("form");
 
 const ulList = document.querySelector("ul");
 
-const li = document.querySelector("li")
+const li = document.querySelector("li");
 const edit = document.querySelector(".edit");
 const delet = document.querySelector(".delete");
 
-function insertList  (e) {
-    e.preventDefault()
-    const createList = document.createElement("li")
-    const createButtonEdit = document.createElement("button")
-    const createButtonSave = document.createElement("button")
-    const createButtonDelete = document.createElement("button")
-    
-    const createText  = document.createElement("p")
-    
+function insertList(e) {
+  e.preventDefault();
+  const createList = document.createElement("li");
+  const createButtonEdit = document.createElement("button");
+  const createButtonSave = document.createElement("button");
+  const createButtonDelete = document.createElement("button");
 
-    // edit button
-    createButtonEdit.textContent = "Edit"
-    createButtonEdit.classList = "edit"
+  const createText = document.createElement("p");
 
-    // save button
-    createButtonSave.textContent = "Save"
-    createButtonSave.classList = "save"
+  // edit button
+  createButtonEdit.textContent = "Edit";
+  createButtonEdit.classList = "edit";
 
+  // save button
+  createButtonSave.textContent = "Save";
+  createButtonSave.classList = "save";
 
-    // delete button
-    createButtonDelete.textContent = "Delete"
-    createButtonDelete.classList = "delete"
+  // delete button
+  createButtonDelete.textContent = "Delete";
+  createButtonDelete.classList = "delete";
 
-    // put input value in list
-    createText.innerText = `${input.value}`
-    createList.append (createText,createButtonEdit,createButtonSave,createButtonDelete)
+  // put input value in list
+  createText.innerText = `${input.value}`;
+  createList.append(
+    createText,
+    createButtonEdit,
+    createButtonSave,
+    createButtonDelete
+  );
 
-    // show to do list
-    ulList.append (createList)
-    input.value = ''
+  // show to do list
+  ulList.append(createList);
+  input.value = "";
 
-    
-    createButtonEdit.addEventListener("click", () => {
-      createText.style.outline = "2px solid white";
-      createText.setAttribute("contenteditable", "true");
+  // edit button click function
+  createButtonEdit.addEventListener("click", () => {
+    createText.style.outline = "2px solid white";
+    createText.setAttribute("contenteditable", "true");
 
-      
-      createButtonEdit.style.display= "none"
-      createButtonSave.style.display = "block"
-      createText.style.cursor = "text"
-    });
+    createButtonEdit.style.display = "none";
+    createButtonSave.style.display = "block";
+    createText.style.cursor = "text";
+  });
 
-    
-    
-    createButtonSave.addEventListener("click",()=>{
-        createText.setAttribute("contenteditable", "false");
-        createText.style.outline = "none";
-        createText.style.cursor = "default"
+  // save button click function
+  createButtonSave.addEventListener("click", () => {
+    createText.setAttribute("contenteditable", "false");
+    createText.style.outline = "none";
+    createText.style.cursor = "default";
 
-        createButtonEdit.style.display= "block"
-        createButtonSave.style.display = "none"
+    createButtonEdit.style.display = "block";
+    createButtonSave.style.display = "none";
+  });
 
-    })
-
-
-    createButtonDelete.addEventListener("click",()=>{
-        createList.style.display = "none"
-    })
+  // delete button click function
+  createButtonDelete.addEventListener("click", () => {
+    createList.style.display = "none";
+  });
 }
 
-form.addEventListener("submit",(e)=>insertList(e));
-
-
-
-
+form.addEventListener("submit", (e) => insertList(e));
