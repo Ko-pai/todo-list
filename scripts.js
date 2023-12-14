50544ddb6e386d7ada9dcb0265ea3b88dad707f0
Pai thet <leontinus866@gmail.com>
2023-12-14 19:56:37 +0630

    first commit

---
+++ scripts.js
@@ -0,0 +1,74 @@
+const input = document.querySelector(".todoTextInput");
+const form = document.querySelector("form");
+
+const ulList = document.querySelector("ul");
+
+const li = document.querySelector("li")
+const edit = document.querySelector(".edit");
+const delet = document.querySelector(".delete");
+
+function insertList  (e) {
+    e.preventDefault()
+    const createList = document.createElement("li")
+    const createButtonEdit = document.createElement("button")
+    const createButtonSave = document.createElement("button")
+    const createButtonDelete = document.createElement("button")
+    
+    const createText  = document.createElement("p")
+    
+
+    // edit button
+    createButtonEdit.textContent = "Edit"
+    createButtonEdit.classList = "edit"
+
+    // save button
+    createButtonSave.textContent = "Save"
+    createButtonSave.classList = "save"
+
+
+    // delete button
+    createButtonDelete.textContent = "Delete"
+    createButtonDelete.classList = "delete"
+
+    // put input value in list
+    createText.innerText = `${input.value}`
+    createList.append (createText,createButtonEdit,createButtonSave,createButtonDelete)
+
+    ulList.append (createList)
+    
+    input.value = ''
+
+    
+    createButtonEdit.addEventListener("click", () => {
+      createText.style.outline = "2px solid white";
+      createText.setAttribute("contenteditable", "true");
+
+      
+      createButtonEdit.style.display= "none"
+      createButtonSave.style.display = "block"
+      createText.style.cursor = "text"
+    });
+
+    
+    
+    createButtonSave.addEventListener("click",()=>{
+        createText.setAttribute("contenteditable", "false");
+        createText.style.outline = "none";
+        createText.style.cursor = "default"
+
+        createButtonEdit.style.display= "block"
+        createButtonSave.style.display = "none"
+
+    })
+
+
+    createButtonDelete.addEventListener("click",()=>{
+        createList.style.display = "none"
+    })
+}
+
+form.addEventListener("submit",(e)=>insertList(e));
+
+
+
+
